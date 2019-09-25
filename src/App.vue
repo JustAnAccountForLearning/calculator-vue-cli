aae5957bb75957a2460c07d0daacdb6ac800c418
Thomas Wood <thomas.j.wood28@gmail.com>
2019-09-25 06:00:52 -0400

    Initial upload after build. Add Calculator.vue.

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calculator msg="Calculated Calculator"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    // Commented out the HelloWorld as it was part of the initial build
+    //HelloWorld,
+    Calculator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="caclulator">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+  props: {
+    msg: String
+  }
+}
+</script>
